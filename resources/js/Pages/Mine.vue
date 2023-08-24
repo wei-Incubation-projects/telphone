2b399ee91c2975a33aa7a1f9bb0526cf719fd71a
weicome <407888190@qq.com>
2023-08-25 01:38:21 +0800

    [Add] 增加页面

--- resources/js/Pages/Mine.vue
+++ resources/js/Pages/Mine.vue
@@ -4,7 +4,20 @@ import { Head } from '@inertiajs/vue3';
 </script>
 <template>
     <Head title="我的"></Head>
-    <div>我的</div>
+    <div class="w-full min-h-screen bg-gray-200 flex flex-col">
+        <van-nav-bar
+            title="个人中心"
+            left-arrow
+            @click-left="onClickLeft"
+        />
+        <van-space class="mt-2" direction="vertical" fill>
+            <van-cell title="拨打记录" is-link />
+
+        </van-space>
+        <div class="mt-8">
+            <van-button class="mt-4" type="danger"  block>退出登录</van-button>
+        </div>
+    </div>
     <TabbarLayout  :active="2"/>
 </template>
 <style scoped>
--- resources/js/Pages/Recall.vue
+++ resources/js/Pages/Recall.vue
@@ -5,7 +5,20 @@ import { Head } from '@inertiajs/vue3';
 
 <template>
     <Head title="复播"/>
-    <div>复播</div>
+    <div class="w-full min-h-screen px-3 bg-gray-200 flex flex-col">
+        <div class="mt-4">
+            <span class="px-1 bg-blue-400 text-sm rounded-md">可复播总量：0</span>
+        </div>
+        <div class="w-full mt-4 text-sm">
+            复播数据是上一次拨打后标记为<span class="text-blue-500">未接通(通话中/语言助手/在忙稍后回电)</span>
+            状态的数据。复播后，不再提供该标记。
+        </div>
+        <div class="w-full h-full flex-1 ">
+            <div class="w-full h-full flex-1 flex justify-center items-center">
+                <van-empty image="search" description="暂无复播数据" />
+            </div>
+        </div>
+    </div>
     <TabbarLayout  :active="1"/>
 </template>
 
